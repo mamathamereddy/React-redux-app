c7358b0fc516e60e41216724f654642abc896f64
mamathamereddy <mamathavangala1@gmail.com>
2020-09-07 12:27:47 +0200

    changing file name

--- shopping-cart-client/src/App.js
+++ shopping-cart-client/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
     <div className="App">
-     <Home/>
+     <Navbar/>
     </div>
   );
 }
