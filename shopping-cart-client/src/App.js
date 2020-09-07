65b888c981798cb2f704fbb715aa60cfb8fce7ba
mamathamereddy <mamathavangala1@gmail.com>
2020-09-07 12:22:05 +0200

    adding category apri and some frontend homefile

--- shopping-cart-client/src/App.js
+++ shopping-cart-client/src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home';
 
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <nav>
-          <h2>Shopping cart</h2>
-          <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Catageory</a></li>
-          <li><a href="#">Cart</a></li>
-          </ul>
-        </nav>
-      </header>
+     <Home/>
     </div>
   );
 }
---
+++ shopping-cart-client/src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <header>
+        <div className="overlay"></div>
+        <nav>
+          <h2>Shopping cart</h2>
+          <ul>
+          <li><a href="#">Men</a></li>
+          <li><a href="#">Women</a></li>
+          <li className="cart"><a href="#">
+          <ion-icon name="basket-outline"></ion-icon>Cart <span>(0)</span></a></li>
+          </ul>
+        </nav>
+      </header>
+    )
+}
+
+export default Home
