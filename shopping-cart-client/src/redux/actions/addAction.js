97a0c897b9a0cdd76687bf9b15d4b235f03a198a
mamathamereddy <mamathavangala1@gmail.com>
2023-03-01 21:29:49 +0100

    adding reducer and action for cart

--- shopping-cart-client/src/redux/actions/addAction.js
+++ shopping-cart-client/src/redux/actions/addAction.js
@@ -1,10 +1,19 @@
-import {ADD_PRODUCTS_BASKET} from './types';
+import { ADD_PRODUCTS_BASKET } from "./types";
 
-export const  addToBasket=()=>{
-    return(dispatch)=>{
-        console.log('Adding to basket');
-        dispatch({
-               type:ADD_PRODUCTS_BASKET
-        })
+export const addToBasket = (product, cartProduct) => (dispatch) => {
+  const cartItems = cartProduct.slice();
+  let productAlreadyInCart = false;
+  const basketProduct = product[0];
+  cartItems.forEach((cart) => {
+    if (cart && basketProduct && cart.id === basketProduct.id) {
+      cart.count += 1;
+      productAlreadyInCart = true;
     }
-}
+  });
+
+  if (!productAlreadyInCart) {
+    cartItems.push({ ...basketProduct, count: 1 });
+  }
+  localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  dispatch({ type: ADD_PRODUCTS_BASKET, cartItems: { cartItems } });
+};
--- shopping-cart-client/src/redux/reducers/basketReducer.js
+++ shopping-cart-client/src/redux/reducers/basketReducer.js
@@ -1,14 +1,22 @@
 import { ADD_PRODUCTS_BASKET, GET_NUMBERS_BASKET } from "../actions/types";
 
+// const initialState = {
+//   basketNumbers: 0,
+//   cartProducts: [],
+// };
+
+const localItems = localStorage.getItem("cartItems");
 const initialState = {
-  basketNumbers: 0,
+  cartProducts: localItems ? JSON.parse(localItems) : [],
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_PRODUCTS_BASKET:
       return {
-        basketNumbers: state.basketNumbers + 1,
+        ...state,
+        cartProducts: action.cartItems.cartItems,
+        //basketNumbers: state.basketNumbers + 1,
       };
     case GET_NUMBERS_BASKET:
       return {
