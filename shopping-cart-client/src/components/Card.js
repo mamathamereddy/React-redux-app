a741b23a70954fbbd1274e7a95bae2af70b107cc
mamathamereddy <mamathavangala1@gmail.com>
2023-03-01 21:33:04 +0100

    refactoring  card-component acccordingly

--- shopping-cart-client/src/components/Card.js
+++ shopping-cart-client/src/components/Card.js
@@ -1,25 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Button from "../components/Button";
 import "../App.css";
 
-const Card = ({ products }) => {
+const Card = ({ products, addToBasket, cartProducts }) => {
   //console.log(products);
+  const handleClick = () => {
+    addToBasket(products, cartProducts);
+  };
   return (
-    <Link
-      to={`/products/${products.id}`}
-      className="products_container"
-      key={products.id}
-    >
-      <ul>
-        <li className="product-image">
-          <img src={products.image} alt={products.name} />
-        </li>
+    <>
+      <Link
+        to={`/products/${products.id}`}
+        className="products_container"
+        key={products.id}
+      >
+        <ul>
+          <li className="product-image">
+            <img src={products.image} alt={products.name} />
+          </li>
 
-        <li>{products.brand}</li>
-        <li>{products.name}</li>
-        <li>{products.price} DKK</li>
-      </ul>
-    </Link>
+          <li>{products.brand}</li>
+          <li>{products.name}</li>
+          <li>{products.price} DKK</li>
+        </ul>
+      </Link>
+      <Button
+        type="button"
+        onClick={handleClick}
+        text="Add to cart"
+        className="cart"
+      />
+    </>
   );
 };
 
