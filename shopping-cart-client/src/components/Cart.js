7a2fa3169af20836e7f2c45253a536096ccb3f38
mamathamereddy <mamathavangala1@gmail.com>
2023-03-03 13:28:59 +0100

    adding the cart component

--- shopping-cart-client/src/components/Cart.js
+++ shopping-cart-client/src/components/Cart.js
@@ -1,11 +1,57 @@
 import React from "react";
+import { connect } from "react-redux";
+import Fade from "react-reveal";
 
-function Cart() {
+const Cart = ({ basketProps }) => {
+  const { cartProducts } = basketProps;
+  console.log(cartProducts);
+
+  const formatCurrency = (num) => {
+    return "$" + Number(num && num.toFixed(1)).toLocaleString() + " ";
+  };
+
   return (
-    <div>
-      <h3>this is cart page</h3>
-    </div>
+    <>
+      <div>
+        {cartProducts.length === 0 ? (
+          <div className="cart cart-header">Cart is empty</div>
+        ) : (
+          <div className="cart cart-header">
+            You have {cartProducts.length} in the cart{" "}
+          </div>
+        )}
+        <h3>this is cart page</h3>
+      </div>
+      <div className="cart">
+        <Fade left cascade>
+          <ul className="cart-items">
+            {cartProducts.map((item) => (
+              <li key={item.id}>
+                <div>
+                  <img src={item.image} alt={item.name}></img>
+                </div>
+                <div>
+                  <div>{item.name}</div>
+                  <div className="right">
+                    {formatCurrency(item.price)} x {item.count}{" "}
+                    <button
+                      className="button"
+                      onClick={() => this.props.removeFromCart(item)}
+                    >
+                      Remove
+                    </button>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </Fade>
+      </div>
+    </>
   );
-}
+};
 
-export default Cart;
+const mapStateToProps = (state) => ({
+  basketProps: state.basketState,
+});
+export default connect(mapStateToProps)(Cart);
