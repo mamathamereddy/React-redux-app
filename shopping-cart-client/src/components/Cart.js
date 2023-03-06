71660ea9cf6b2b15f7dd2b54448f05655d2d9642
mamathamereddy <mamathavangala1@gmail.com>
2023-03-06 13:33:53 +0100

    adding cart component

--- shopping-cart-client/src/components/Cart.js
+++ shopping-cart-client/src/components/Cart.js
@@ -1,64 +1,143 @@
 import React from "react";
 import { connect } from "react-redux";
-import Fade from "react-reveal";
 
-const Cart = ({ basketProps }) => {
+import { Link } from "react-router-dom";
+import { increaseProductCount } from "../redux/actions/addAction";
+
+import {
+  decreaseProductCount,
+  removeFromCart,
+} from "../redux/actions/deleteAction";
+
+const Cart = ({
+  basketProps,
+  removeFromCart,
+  decreaseProductCount,
+  increaseProductCount,
+}) => {
   const { cartProducts } = basketProps;
-  console.log(cartProducts);
 
-  const formatCurrency = (num) => {
-    return Number(num && num.toFixed(1)).toLocaleString() + "Dkk ";
-  };
+  const totalAmount = cartProducts.reduce((acc, curr) => {
+    return curr.count * curr.price + acc;
+  }, 0);
 
   return (
-    <>
-      <div>
-        {cartProducts.length === 0 ? (
-          <div className="carts-header">Cart is empty</div>
-        ) : (
-          <div className="carts-header">
-            You have{" "}
-            <span>
-              {cartProducts.length}
-              {""}
-            </span>{" "}
-            items in the cart{" "}
+    <div className="cart-container">
+      {cartProducts && cartProducts.length === 0 ? (
+        <div className="cart-empty">
+          <p>Your cart is currently empty</p>
+          <div className="start-shopping">
+            <Link to="/">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="currentColor"
+                className="bi bi-arrow-left"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+                />
+              </svg>
+              <span>Start Shopping</span>
+            </Link>
           </div>
-        )}
-        <h3>this is cart page</h3>
-      </div>
-      <div className="cart">
-        <Fade left cascade>
-          <ul className="cart-items">
-            {cartProducts.map((item) => (
-              <li key={item.id}>
-                <div>
-                  <img src={item.image} alt={item.name}></img>
-                </div>
-                <div>
-                  <div>{item.name}</div>
-                  <div className="right">
-                    {formatCurrency(item.price)} x {item.count}
-                    {" ="}
-                    {formatCurrency(item.price * item.count)}
+        </div>
+      ) : (
+        <div>
+          <div className="titles">
+            <h3 className="product-title">Product</h3>
+            <h3 className="price">Price</h3>
+            <h3 className="quantity">Quantity</h3>
+            <h3 className="total">Total</h3>
+          </div>
+          <div className="cart-items">
+            {cartProducts &&
+              cartProducts.map((cartItem) => (
+                <div className="cart-item" key={cartItem.id}>
+                  <div className="cart-product">
+                    <img src={cartItem.image} alt={cartItem.name} />
+                    <div>
+                      <h3>{cartItem.name}</h3>
+
+                      <button
+                        onClick={() => removeFromCart(cartItem, cartProducts)}
+                      >
+                        Remove
+                      </button>
+                    </div>
+                  </div>
+                  <div className="cart-product-price">${cartItem.price}</div>
+                  <div className="cart-product-quantity">
                     <button
-                      className="button"
-                      onClick={() => this.props.removeFromCart(item)}
+                      onClick={() =>
+                        decreaseProductCount(cartItem, cartProducts)
+                      }
                     >
-                      Remove
+                      -
+                    </button>
+                    <div className="count">{cartItem.count}</div>
+                    <button
+                      onClick={() =>
+                        increaseProductCount(cartItem, cartProducts)
+                      }
+                    >
+                      +
                     </button>
                   </div>
+                  <div className="cart-product-total-price">
+                    ${(cartItem.price * cartItem.count).toFixed(2)}
+                  </div>
                 </div>
-              </li>
-            ))}
-          </ul>
-        </Fade>
-      </div>
-    </>
+              ))}
+          </div>
+          <div className="cart-summary">
+            <button
+              className="clear-btn"
+              onClick={() => console.log("clear cart")}
+            >
+              Clear Cart
+            </button>
+            <div className="cart-checkout">
+              <div className="subtotal">
+                <span>Subtotal</span>
+                <span className="amount">{totalAmount.toFixed(2)}</span>
+              </div>
+              <p>Taxes and shipping calculated at checkout</p>
+              <button>Check out</button>
+              <div className="continue-shopping">
+                <Link to="/">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                    className="bi bi-arrow-left"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+                    />
+                  </svg>
+                  <span>Continue Shopping</span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
 const mapStateToProps = (state) => ({
   basketProps: state.basketState,
 });
-export default connect(mapStateToProps)(Cart);
+export default connect(mapStateToProps, {
+  increaseProductCount,
+  removeFromCart,
+  decreaseProductCount,
+})(Cart);
